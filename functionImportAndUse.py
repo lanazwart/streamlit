677dd4971ca6a2e720861fd0ca1719d8f5fbb992
Lana Zwart <lanazwart@gmail.com>
2024-12-10 14:01:38 +0000

    Update and rename functionImportAndUse to functionImportAndUse.py

---
+++ functionImportAndUse.py
@@ -0,0 +1,15 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(":cup_with_straw: Customise your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want i your custom Smoothie
+    """)
+
+from snowflake.snowpark.functions import col
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
